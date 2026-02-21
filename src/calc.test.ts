dc0c77558a3f36484b312adb56ea11e7ea91cddf
Rojas Barrientos, Mauricio (Externos - Amaris) <mauricio.rojasbarrientos@externos-cl.cencosud.com>
2026-02-21 19:07:26 -0300

    refactor: improve unit test

--- src/calc.test.ts
+++ src/calc.test.ts
@@ -1,6 +1,25 @@
 import { add } from './calc';
 
 describe('calculator', () => {
+    beforeEach(() => {
+        // setup code if needed
+        console.log('BeforeEach called');
+    });
+    afterEach(() => {
+        // teardown code if needed
+        console.log('AfterEach called');
+    })
+    describe('should perform addition', () => {
+        it('adds two positive numbers', () => {
+            expect(add(1, 2)).toEqual(3);
+        });
+        it('adds two negatives numbers', () => {
+            expect(add(-1, -2)).toEqual(-3);
+        });
+        it('adds one positive and one negative numbers', () => {
+            expect(add(1, -2)).toEqual(-1);
+        });
+    })
     it('should be able to add two numbers', () => {
         expect(add(1, 2)).toEqual(3);
     });
