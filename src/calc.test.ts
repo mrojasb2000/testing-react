40f7e8a3c4ea481b2ce36290efc353712279cf45
Rojas Barrientos, Mauricio (Externos - Amaris) <mauricio.rojasbarrientos@externos-cl.cencosud.com>
2026-02-21 22:38:18 -0300

    refactor: add more unit test

--- src/calc.test.ts
+++ src/calc.test.ts
@@ -28,25 +28,36 @@ describe('calculator', () => {
     it('should return true when four number not equals to three number', () => {
         expect(4).not.toEqual(3);
     });
-    it('should return true when string match with patterns', () => {
-        expect("juntao").toMatch(/\w+/);
+    describe('matchers for patterns', () => {
+        it('should return true when string match with patterns', () => {
+            expect("juntao").toMatch(/\w+/);
+        });
+        it('should return true when number match with patterns', () => {
+            expect("185-3345-3343").toMatch(/^\d{3}-\d{4}-\d{4}$/);
+        });
+        it('should return false when number not match with patterns', () => {
+            expect("1853-3345-3343").not.toMatch(/^\d{3}-\d{4}-\d{4}$/);
+        });
     });
-    it('should return true when number match with patterns', () => {
-        expect("185-3345-3343").toMatch(/^\d{3}-\d{4}-\d{4}$/);
+    describe('matchers for compare', () => {
+        it('should return true when 1 + 2 to be greater than 2', () => {
+            expect(1 + 2).toBeGreaterThan(2);
+        });
+        it('should return true when 1 + 2 to be greater than or equal to 2', () => {
+            expect(1 + 2).toBeGreaterThanOrEqual(2);
+        });
+        it('should return true when 1 + 2 to be less than 4', () => {
+            expect(1 + 2).toBeLessThan(4);
+        });
+        it('should return true when 1 + 2 to be less than or equal to 4', () => {
+            expect(1 + 2).toBeLessThanOrEqual(4);
+        });
     });
-    it('should return false when number not match with patterns', () => {
-        expect("1853-3345-3343").not.toMatch(/^\d{3}-\d{4}-\d{4}$/);
-    });
-    it('should return true when 1 + 2 to be greater than 2', () => {
-        expect(1 + 2).toBeGreaterThan(2);
-    });
-    it('should return true when 1 + 2 to be greater than or equal to 2', () => {
-        expect(1 + 2).toBeGreaterThanOrEqual(2);
-    });
-    it('should return true when 1 + 2 to be less than 4', () => {
-        expect(1 + 2).toBeLessThan(4);
-    });
-    it('should return true when 1 + 2 to be less than or equal to 4', () => {
-        expect(1 + 2).toBeLessThanOrEqual(4);
+    describe('matchers for arrays and objects', () => {
+        it('should return true when array contains string', () => {
+            const users = ["Juntao", "Abruzzi", "Alex"];
+            expect(users).toContainEqual('Juntao');
+            expect(users).toContain(users[1]);
+        });
     });
 });
