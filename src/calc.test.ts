1fa19292162217769c29be5715336a97e7ed0396
Rojas Barrientos, Mauricio (Externos - Amaris) <mauricio.rojasbarrientos@externos-cl.cencosud.com>
2026-02-21 22:23:10 -0300

    refactor: add more unit test

--- src/calc.test.ts
+++ src/calc.test.ts
@@ -11,7 +11,9 @@ describe('calculator', () => {
     })
     describe('should perform addition', () => {
         it('adds two positive numbers', () => {
-            expect(add(1, 2)).toEqual(3);
+            const actual: number = add(1, 2);
+            const expected: number = 3;
+            expect(actual).toEqual(expected);
         });
         it('adds two negatives numbers', () => {
             expect(add(-1, -2)).toEqual(-3);
@@ -22,5 +24,17 @@ describe('calculator', () => {
     })
     it('should be able to add two numbers', () => {
         expect(add(1, 2)).toEqual(3);
+    });
+    it('should return true when four number not equals to three number', () => {
+        expect(4).not.toEqual(3);
+    });
+    it('should return true when string match with patterns', () => {
+        expect("juntao").toMatch(/\w+/);
+    });
+    it('should return true when number match with patterns', () => {
+        expect("185-3345-3343").toMatch(/^\d{3}-\d{4}-\d{4}$/);
+    });
+    it('should return false when number not match with patterns', () => {
+        expect("1853-3345-3343").not.toMatch(/^\d{3}-\d{4}-\d{4}$/);
     });
 });
