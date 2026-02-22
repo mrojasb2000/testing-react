67e0df6ca053e31fa3f6f78c50bffc24c9b37866
Rojas Barrientos, Mauricio (Externos - Amaris) <mauricio.rojasbarrientos@externos-cl.cencosud.com>
2026-02-22 13:22:19 -0300

    refactor: add expect extend function

--- src/json.test.ts
+++ src/json.test.ts
@@ -30,24 +30,53 @@ describe('jest matchers', () => {
             });
             expect(user).toEqual(matcher);
         });
-        it('should return array when jsonpath query get projects', () => {
-            const actual = jsonpath.query(user, '$.projects');
-            const expected = 1;
-            expect(actual.length).toBe(expected);
+        describe('jsonpath matchers', () => {
+            it('should return array when jsonpath query get projects', () => {
+                const actual = jsonpath.query(user, '$.projects');
+                const expected = 1;
+                expect(actual.length).toBe(expected);
+            });
+            it('should return array with one element when jsonpath query get first project', () => {
+                const actual = jsonpath.query(user, '$.projects[0]');
+                expect(actual.length).toBe(1);
+            });
+            it('should return array with property name when jsonpath query get first project', () => {
+                const actual = jsonpath.query(user, '$.projects[0].name');
+                const expected: string[] = ['ThoughtWorks University'];
+                expect(actual).toEqual(expected);
+            });
+            it("should return empty array when jsonpath query didn't match anything", () => {
+                const actual = jsonpath.query(user, '$.projects[0].address');
+                const expected: number = 0;
+                expect(actual.length).toBe(expected);
+            });
         });
-        it('should return array with one element when jsonpath query get first project', () => {
-            const actual = jsonpath.query(user, '$.projects[0]');
-            expect(actual.length).toBe(1);
-        });
-        it('should return array with property name when jsonpath query get first project', () => {
-            const actual = jsonpath.query(user, '$.projects[0].name');
-            const expected: string[] = ['ThoughtWorks University'];
-            expect(actual).toEqual(expected);
-        });
-        it("should return empty array when jsonpath query didn't match anything", () => {
-            const actual = jsonpath.query(user, '$.projects[0].address');
-            const expected: number = 0;
-            expect(actual.length).toBe(expected);
+        describe('extend the expect function', () => {
+            expect.extend({
+                toMatchJsonPath(received: any, argument: any) {
+                    const result = jsonpath.query(received, argument);
+                    if (result.length > 0) {
+                        return {
+                            pass: true,
+                            message: () => 'matched',
+                        };
+                    } else {
+                        return {
+                            pass: false,
+                            message: () => `expected ${JSON.stringify(received)} to match ${argument}`,
+                        }
+                    }
+                },
+            });
+            describe('jsonpath', () => {
+                it('should return successfully when json contains username', () => {
+                    const user = {
+                        name: 'Juntao',
+                    };
+                    // @ts-ignore
+                    expect(user).toMatchJsonPath('$.name');
+                });
+            });
         });
     });
 });
